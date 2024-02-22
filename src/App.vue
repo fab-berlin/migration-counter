95ed0b768e153f79ba737d368a14964a8fc796d5
Fabian Dahms <fab_d2@yahoo.de>
2024-02-22 21:57:18 +0100

    added data file, added first functionality

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,42 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <h1>{{current}} / {{total}}</h1><br>
+    <h1>{{percent}}%</h1>
 </template>
 
+<script>
+    import axios from 'axios';
+    import {ref} from 'vue';
+
+    export default {
+        name: 'App',
+        setup() {
+            const total = ref();
+            const current = ref();
+            const percent = ref();
+
+            axios.get('./data.csv').then(response => {
+                const data = response.data.split('\n').reverse();
+                processMigrationData(data);
+            })
+
+            const processMigrationData = (data) => {
+                total.value = parseInt(data[0].split(',')[1]);
+                current.value = parseInt(data[0].split(',')[0]);
+                percent.value = Math.ceil(current.value/total.value*100);
+            }
+
+
+            return {
+                total,
+                current,
+                percent
+            }
+        }
+    }
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+    h1 {
+        font-size: 24px;
+    }
 </style>
